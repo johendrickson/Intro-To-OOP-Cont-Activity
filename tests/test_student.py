d12cd5e44550a7d969fe59ea332ad5e3d38adead
marisela <mariselaalejandragomez@gmail.com>
2025-04-01 11:41:23 -0700

    created 5 test cases for the student class for the functions add_class, get_num_classes and get_summary. Not tested for correct implementation.
    Co-authored-by: Jamie Hendrickson <johendrickson@users.noreply.github.com>

--- tests/test_student.py
+++ tests/test_student.py
@@ -1,3 +1,34 @@
 from school_schedule.student import Student
 
-# Write your tests here!
+
+def test_add_class_student():
+    student = Student("Bob", "sophmore", ["math", "science"])
+    student.add_class("history")
+    assert student.classes == ["math", "science", "history"]
+
+
+def test_add_class_empty():
+    student = Student("Jamie", "senior", [])
+    student.add_class("home ec")
+    assert student.classes == ["home ec"]
+
+
+def test_get_num_classes():
+    student = Student("Bob", "sophmore", ["math", "science"])
+    result = student.get_num_classes()
+    assert result == 3
+
+
+def test_get_num_clssses_empty():
+    student = Student("Jamie", "senior", [])
+    result = student.get_num_classes()
+    assert result == 0
+
+
+def test_get_summary():
+    student = Student("Sela", "freshman", ["art", "physics"])
+    result = (f"{student.name} is a {student.grade} "
+              f"enrolled in {student.get_num_classes()} classes: "
+              f"{student.display_classes()}")
+
+    assert result == "Sela is freshman enrolled in 2 classes: art, physics"
